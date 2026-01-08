4e77947f98179332bb304e1f90d8b786062dec74
Borislav Minev <bminev756@gmail.com>
2026-01-08 16:43:46 +0200

    feat: add initial PostgreSQL migrations

--- src/TaskManagementSystem.WebApi/obj/Debug/net9.0/TaskManagementSystem.WebApi.AssemblyInfo.cs
+++ src/TaskManagementSystem.WebApi/obj/Debug/net9.0/TaskManagementSystem.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementSystem.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d72bb226c7a83064a2cf20600296a7e344657196")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d07deee4a702b259c01b4c2b83a1e7d8c8c70c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementSystem.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementSystem.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
